5f5c544589e2dc3dc1e04d890b972349ba8f626c
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2023-11-05 16:04:11 +0600

    basic folder structure setup added

---
+++ src/app/(auth)/(routes)/sign-in/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SignInPage = () => {
+  return (
+    <div>
+      <p>This is an unprotected page</p>
+    </div>
+  );
+};
+export default SignInPage;
---
+++ src/app/(dashboard)/(routes)/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl mb-5">This a protected page</p>
+      <Button>Shadcn Worked</Button>
+    </div>
+  );
+}
