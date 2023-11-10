e6db9af3f639daaed3fde5c6fe2768c1fe3bb4ea
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-10 09:49:15 +0600

    photo upload feature added

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.js
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.js
@@ -7,6 +7,7 @@ import { redirect } from "next/navigation";
 import { IconBadge } from "@/components/icon-badge";
 import db from "@/lib/db";
 import { DescriptionForm } from "./_components/description-form";
+import { ImageForm } from "./_components/image-form";
 import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }) => {
@@ -66,6 +67,7 @@ const CourseIdPage = async ({ params }) => {
                         initialData={course}
                         courseId={course.id}
                     />
+                    <ImageForm initialData={course} courseId={course.id} />
                 </div>
             </div>
         </div>
