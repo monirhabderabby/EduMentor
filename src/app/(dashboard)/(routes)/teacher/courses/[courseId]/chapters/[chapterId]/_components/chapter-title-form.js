334b499413b7dff17e23f3a675d29bd3ce4bcb9b
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2023-11-18 07:55:34 +0600

    Chapter title edit form developed

---
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-title-form.js
@@ -0,0 +1,113 @@
+"use client";
+
+// packages
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import * as z from "zod";
+
+// components
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Pencil } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+// Form Schema
+const formSchema = z.object({
+    title: z.string().min(1),
+});
+
+export const ChapterTitleForm = ({ initialData, courseId, chapterId }) => {
+    // state
+    const [isEditing, setIsEditing] = useState(false);
+
+    // hooks
+    const router = useRouter();
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    });
+
+    // JS Variables
+    const { isSubmitting, isValid } = form.formState;
+
+    // functions
+    const toggleEdit = () => setIsEditing((current) => !current);
+
+    const onSubmit = async (values) => {
+        try {
+            await axios.patch(
+                `/api/courses/${courseId}/chapters/${chapterId}`,
+                values
+            );
+            toast.success("Chapter Updated");
+            toggleEdit();
+            router.refresh();
+        } catch {
+            toast.error("Something went wrong");
+        }
+    };
+
+    return (
+        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+            <div className="font-medium flex items-center justify-between">
+                Chapter Title
+                <Button onClick={toggleEdit} variant="ghost">
+                    {isEditing ? (
+                        <>Cancel</>
+                    ) : (
+                        <>
+                            <Pencil className="h-4 w-4 mr-2" />
+                            Edit Title
+                        </>
+                    )}
+                </Button>
+            </div>
+
+            {!isEditing && <p className="text-sm mt-2">{initialData.title}</p>}
+
+            {isEditing && (
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4 mt-4"
+                    >
+                        <FormField
+                            control={form.control}
+                            name="title"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder="e.g. 'Introduction to the course...'"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex items-center gap-x-2">
+                            <Button
+                                type="submit"
+                                disabled={!isValid || isSubmitting}
+                            >
+                                Save
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            )}
+        </div>
+    );
+};
