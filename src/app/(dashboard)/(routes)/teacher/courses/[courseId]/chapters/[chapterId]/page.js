6bfaeabf05313131466b94ebf4d1b1b6e429ac0f
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2023-11-18 15:54:43 +0600

    [DONE]: Chapter access form

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.js
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.js
@@ -1,12 +1,13 @@
 // packages
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
 // components
 import { IconBadge } from "@/components/icon-badge";
 import db from "@/lib/db";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 
@@ -83,6 +84,17 @@ const ChapterIdPage = async ({ params }) => {
                             initialData={chapter}
                             courseId={params.courseId}
                             chapterId={params.chapterId}
+                        />
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={Eye} />
+                            <h2 className="text-xl">Access Settings</h2>
+                        </div>
+                        <ChapterAccessForm
+                            initialData={chapter}
+                            chapterId={params.chapterId}
+                            courseId={params.courseId}
                         />
                     </div>
                 </div>
