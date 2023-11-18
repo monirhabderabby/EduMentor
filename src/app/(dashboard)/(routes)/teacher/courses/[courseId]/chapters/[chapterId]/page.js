cbd9da16ca6300a9e6edde1979c54c2662cf598a
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2023-11-18 18:29:59 +0600

    [DONE]: Mux setup, chapter video upload and play feature

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.js
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.js
@@ -1,6 +1,6 @@
 // packages
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
@@ -10,6 +10,7 @@ import db from "@/lib/db";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterVideoForm } from "./_components/chapter-video-form";
 
 const ChapterIdPage = async ({ params }) => {
     // hooks
@@ -97,6 +98,17 @@ const ChapterIdPage = async ({ params }) => {
                             courseId={params.courseId}
                         />
                     </div>
+                </div>
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={Video} />
+                        <h2 className="text-xl">Add a video</h2>
+                    </div>
+                    <ChapterVideoForm
+                        initialData={chapter}
+                        courseId={params.courseId}
+                        chapterId={params.chapterId}
+                    />
                 </div>
             </div>
         </div>
