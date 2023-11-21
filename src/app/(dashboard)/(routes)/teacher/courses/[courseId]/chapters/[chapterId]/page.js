893aec11e4b02dc32f60da1e925b87461f217ebe
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-11-21 19:58:45 +0600

    added: chapter preview makring setting

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.js
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.js
@@ -1,12 +1,13 @@
 // packages
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
 // components
 import { IconBadge } from "@/components/icon-badge";
 import db from "@/lib/db";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 
@@ -84,6 +85,23 @@ const ChapterIdPage = async ({ params }) => {
                             courseId={params.courseId}
                             chapterId={params.chapterId}
                         />
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={Eye} />
+                            <h2 className="text-xl">Access Setting</h2>
+                        </div>
+                        <ChapterAccessForm
+                            initialData={chapter}
+                            courseId={params.courseId}
+                            chapterId={params.chapterId}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={Video} />
+                        <h2 className="text-xl">Add a video</h2>
                     </div>
                 </div>
             </div>
