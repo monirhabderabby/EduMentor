0e78847fcd662a3f0cf87051456fc9b463d1ad18
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2023-11-29 22:10:01 +0600

    logo updated

--- src/app/(dashboard)/_components/Logo.js
+++ src/app/(dashboard)/_components/Logo.js
@@ -3,7 +3,13 @@ import Image from "next/image";
 
 const Logo = () => {
     return (
-        <Image height={130} width={130} alt="logo" src="/logo.svg" priority />
+        <Image
+            height={130}
+            width={130}
+            alt="logo"
+            src="/edumentor-logo.svg"
+            priority
+        />
     );
 };
 
